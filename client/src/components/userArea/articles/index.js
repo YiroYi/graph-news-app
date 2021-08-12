749f846c9f91ff1fd0ad19239bf1cf7fda0a4de9
YiroYi <yiroyi@live.com>
2021-08-13 01:48:41 +0900

    Adding display User posts

--- client/src/components/userArea/articles/index.js
+++ client/src/components/userArea/articles/index.js
@@ -1,12 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useReducer, useEffect } from "react";
 import UserAreaHOC from "../../hoc/userAreaHoc";
+import { Table, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserPosts } from "../../../store/actions";
 
-const Articles = () => {
+const Articles = (props) => {
+  const [sort, setSort] = useReducer(
+    (state, newState) => ({ ...state, newState }),
+    { limit: 3, order: "desc", sortBy: "_id", skip: 0 }
+  );
+
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserPosts(sort, [], user.auth._id));
+  }, []);
+
   return (
     <Fragment>
       <UserAreaHOC>
         <div className="mt-3">
-          Welcome to your articles
+          <Button
+            onClick={() => {
+              let skip = sort.skip + sort.limit;
+              dispatch(
+                getUserPosts({ ...sort, skip: skip }, user.posts, user.auth._id)
+              ).then(() => {setSort({skip:skip})});
+            }}
+          >
+            Load More
+          </Button>
         </div>
       </UserAreaHOC>
     </Fragment>
