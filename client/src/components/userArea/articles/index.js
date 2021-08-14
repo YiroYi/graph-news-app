535f822adf2a06600436087b9669d844f2ed8b83
YiroYi <yiroyi@live.com>
2021-08-14 11:24:45 +0900

    Adding table to display post

--- client/src/components/userArea/articles/index.js
+++ client/src/components/userArea/articles/index.js
@@ -20,17 +20,46 @@ const Articles = (props) => {
   return (
     <Fragment>
       <UserAreaHOC>
+        <Table striped bordered hover>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+          <tbody>
+            {user.posts
+              ? user.posts.map((post, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{post.title}</td>
+                    <td>{post.category.name}</td>
+                    <td className={post.status === "DRAFT" ? "yell" : "green"}>
+                      {post.status}
+                    </td>
+                    <td className="remove_btn">Remove</td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
+        </Table>
         <div className="mt-3">
           <Button
             onClick={() => {
               let skip = sort.skip + sort.limit;
               dispatch(
                 getUserPosts({ ...sort, skip: skip }, user.posts, user.auth._id)
-              ).then(() => {setSort({skip:skip})});
+              ).then(() => {
+                setSort({ skip: skip });
+              });
             }}
           >
             Load More
           </Button>
+          <Button className="ml-2" variant="outline-info" onClick={()=>{
+            props.history.push('/user_area/create')
+          }}>Create new article</Button>
         </div>
       </UserAreaHOC>
     </Fragment>
